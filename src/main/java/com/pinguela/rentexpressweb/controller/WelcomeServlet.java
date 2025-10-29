ff0df49301e27030f9092968ced30f73b4d819c5
licontera345 <liconterapelopo@gmail.com>
2025-10-29 11:21:22 +0100

    Add public search filters for headquarters availability

--- src/main/java/com/pinguela/rentexpressweb/controller/WelcomeServlet.java
+++ src/main/java/com/pinguela/rentexpressweb/controller/WelcomeServlet.java
@@ -1,6 +1,12 @@
 package com.pinguela.rentexpressweb.controller;
 
+import com.pinguela.rentexpres.dao.HeadquartersDAO;
+import com.pinguela.rentexpres.dao.impl.HeadquartersDAOImpl;
+import com.pinguela.rentexpres.exception.DataException;
+import com.pinguela.rentexpres.model.HeadquartersDTO;
+import com.pinguela.rentexpres.util.JDBCUtils;
 import com.pinguela.rentexpressweb.constants.AppConstants;
+import com.pinguela.rentexpressweb.constants.VehicleConstants;
 import com.pinguela.rentexpressweb.security.RememberMeManager;
 import com.pinguela.rentexpressweb.security.SessionManager;
 import com.pinguela.rentexpressweb.util.Views;
@@ -11,6 +17,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Servlet implementation class WelcomeServlet
@@ -18,6 +31,10 @@ import java.io.IOException;
 @WebServlet("/app/welcome")
 public class WelcomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
+    private static final Logger LOGGER = LogManager.getLogger(WelcomeServlet.class);
+
+    private final HeadquartersDAO headquartersDAO = new HeadquartersDAOImpl();
 
     public WelcomeServlet() {
         super();
@@ -31,6 +48,7 @@ public class WelcomeServlet extends HttpServlet {
         exposeFlashMessage(request);
 
         request.setAttribute(AppConstants.ATTR_PAGE_TITLE, "Bienvenido");
+        request.setAttribute(VehicleConstants.ATTR_HEADQUARTERS, loadHeadquarters());
 
         request.getRequestDispatcher(Views.PUBLIC_WELCOME).forward(request, response);
     }
@@ -53,6 +71,23 @@ public class WelcomeServlet extends HttpServlet {
         if (error != null) {
             request.setAttribute(AppConstants.ATTR_FLASH_ERROR, error);
             SessionManager.removeAttribute(request, AppConstants.ATTR_FLASH_ERROR);
+        }
+    }
+
+    private List<HeadquartersDTO> loadHeadquarters() {
+        Connection connection = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            JDBCUtils.beginTransaction(connection);
+            List<HeadquartersDTO> list = headquartersDAO.findAll(connection);
+            JDBCUtils.commitTransaction(connection);
+            return list;
+        } catch (SQLException | DataException ex) {
+            JDBCUtils.rollbackTransaction(connection);
+            LOGGER.error("Error al recuperar las sedes para la portada", ex);
+            return new ArrayList<>();
+        } finally {
+            JDBCUtils.close(connection);
         }
     }
 }
