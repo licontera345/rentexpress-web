c1282ac07a0ab2f8f229f4959928d11e832a208b
licontera345 <liconterapelopo@gmail.com>
2025-10-31 10:58:21 +0100

    Refactor activity tracker entry into dedicated class

--- src/main/java/com/pinguela/rentexpressweb/controller/HomeServlet.java
+++ src/main/java/com/pinguela/rentexpressweb/controller/HomeServlet.java
@@ -9,10 +9,10 @@ import com.pinguela.rentexpressweb.constants.AppConstants;
 import com.pinguela.rentexpressweb.constants.SecurityConstants;
 import com.pinguela.rentexpressweb.security.SessionManager;
 import com.pinguela.rentexpressweb.service.user.UserProfileService;
-import com.pinguela.rentexpressweb.util.MessageResolver;
+import com.pinguela.rentexpressweb.util.ActivityEntry;
 import com.pinguela.rentexpressweb.util.ControllerUtils;
+import com.pinguela.rentexpressweb.util.MessageResolver;
 import com.pinguela.rentexpressweb.util.UserActivityTracker;
-import com.pinguela.rentexpressweb.util.UserActivityTracker.ActivityEntry;
 import com.pinguela.rentexpressweb.util.Views;
 
 import jakarta.servlet.ServletException;
